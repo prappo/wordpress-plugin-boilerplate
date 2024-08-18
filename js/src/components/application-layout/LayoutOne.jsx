835ccca96205df180f6a7db855f50e8a93231971
Prappo <prappo.prince@gmail.com>
2024-08-18 14:01:38 +0600

    fix style

--- js/src/components/application-layout/LayoutOne.jsx
+++ js/src/components/application-layout/LayoutOne.jsx
@@ -173,7 +173,7 @@ export default function LayoutOne() {
                     </DropdownMenu>
                 </header>
                 }
-                <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+                <main>
                     <Outlet />
                 </main>
             </div>
