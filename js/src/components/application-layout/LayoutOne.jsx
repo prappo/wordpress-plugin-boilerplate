9260f624d5a7832191116f7177060430faa84fbc
Prappo <prappo.prince@gmail.com>
2024-10-04 13:56:50 +0600

    Update LayoutOne.jsx

--- js/src/components/application-layout/LayoutOne.jsx
+++ js/src/components/application-layout/LayoutOne.jsx
@@ -75,7 +75,7 @@ export default function LayoutOne() {
             {showApplicationLayout && <div className="hidden border-r bg-muted/40 md:block">
                 <div className="flex h-full max-h-screen flex-col gap-2">
                     <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
-                        <a href="/" className="flex items-center gap-2 font-semibold">
+                        <a href="#/dashboard" className="flex items-center gap-2 font-semibold">
                             <Logo />
                             <span className="">Plugin Name</span>
                         </a>
