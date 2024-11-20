5a66137fe5704eafb42e3fe7fda5683142c8a9ba
Prappo <prappo.prince@gmail.com>
2024-11-20 12:56:34 +0600

    add build file

---
+++ assets/blocks/block-1/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '397ef6e06862ec3ffe04');
---
+++ assets/blocks/block-1/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '5e309f26cceac88b1e8d');
